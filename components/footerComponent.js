b99fa239a13143ef19a69b1487e81f64f18a738e
Rachita Saha <rachita19082@iiitd.ac.in>
2021-06-19 15:13:34 +0530

    CI Build test

--- components/footerComponent.js
+++ components/footerComponent.js
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+import Head from "next/head";
 
 export default function footerComponent(props) {
   const [footerClass, setFooterClass] = useState("masthead pt-3 mt-3");
@@ -8,7 +9,7 @@ export default function footerComponent(props) {
   }, []);
 
   function setTopMargin() {
-    if (props.noTopMargin) setFooterClass("masthead pt-3");
+    if (props.noTopMargin) setFooterClass('masthead pt-3');
   }
 
   return (
